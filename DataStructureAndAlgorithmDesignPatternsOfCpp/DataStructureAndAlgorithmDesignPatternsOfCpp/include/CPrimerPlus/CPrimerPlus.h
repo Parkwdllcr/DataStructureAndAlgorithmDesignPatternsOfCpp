fad474853c41aa65dd9b2598794b49d73e26e6b4
Parkwdllcr <1441641216@qq.com>
2021-12-14 14:45:33 +0800

    change some CStudyCPrimerPlus

--- DataStructureAndAlgorithmDesignPatternsOfCpp/DataStructureAndAlgorithmDesignPatternsOfCpp/include/CPrimerPlus/CPrimerPlus.h
+++ DataStructureAndAlgorithmDesignPatternsOfCpp/DataStructureAndAlgorithmDesignPatternsOfCpp/include/CPrimerPlus/CPrimerPlus.h
@@ -1,8 +1,12 @@
-#include"CPrimerPlus/CPublicClass.h"
+#pragma once
 #include<iostream>
 #include<climits>
 #include<list>
 #include<string>
+
+#define STUDY_OK    1
+#define STUDY_FAIL    0
+
 
 
 class CStudyCPrimerPlus
@@ -10,29 +14,36 @@ class CStudyCPrimerPlus
 public:
 	//构造函数，重载构造函数，拷贝构造函数，赋值函数，析构函数
 	CStudyCPrimerPlus();
+	CStudyCPrimerPlus(int iSize);
     ~CStudyCPrimerPlus();
 
 private:
+	//拷贝构造函数和赋值函数，如果没有用到，就不需要去写
+	//分为浅拷贝和深拷贝
+	//如果属性中有指针（动态内存），要使用深拷贝，
+	//之后加了线程的，就使用深拷贝进行练习
+	CStudyCPrimerPlus(const CStudyCPrimerPlus& otherCPrimer);
+	CStudyCPrimerPlus& operator=(const CStudyCPrimerPlus& CStudyCPPObject);
 
 public:
 	//第三章
-	template<typename T>
-	void PrintAndComputer(const T& showAndSize);
-    void ComputSizeOfVar();
 	//重载一下各个基本类型
 	void SimpleFourArithmetic(double dbTestOne, double dbTestTwo, int iCalculationType);
     void StudyConstChar(const char* pConstCharConst,std::string strName);
-	template<typename T>
-	T Power(T tBase, int iExponent);
-	
-	void ClassToOtherClass();
 
+	template<typename T>
+	int Power(T &tResult, int iExponent);
+	void ClassToOtherClass();
 public:
 	//第四章
+
+	//注意这几种区别
 	void  GetMemory(char* pChar, int iNum);
 	void  GetMemory(char** pChar, int iNum);
 	char* GetMemory(int inum);
+	//得到一个野指针
 	char* GetString(void);
+
 private:
 	template<typename T>
 	void ShowAndSize(const T& testVar);
@@ -40,27 +51,33 @@ private:
 
 	//初始化类中的常量和static
 private:
-	std::list<int>          m_listTest;
-    const   int m_iSize;
+	int                      m_iCout;
+	std::list<int>           m_listTest;
+    const int                m_iSize;
+	//初始化static类型的类属性
 	static int               m_iStaticTest;
 	const static int         m_iConstStatic;
 	const static std::string m_strTestName;
+private:
+	enum enComputeChar
+	{
+		ADDITION,
+		SUBTRACTION,
+		MULTIPLICATION,
+		DIVIDE,
+		MODULO
+	};
 
 };
 
-template<typename T>
-void CStudyCPrimerPlus::PrintAndComputer(const T& showAndSize)
-{
-	std::cout << "The Test Value is: " << showAndSize<< std::endl;
-	std::cout << "The Test Size  is: " << sizeof(showAndSize) << std::endl;
-}
 
 template<typename T>
-T CStudyCPrimerPlus::Power(T tBase, int iExponent)
+int CStudyCPrimerPlus::Power(T &tResult, int iExponent)
 {
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::Power");
 	try
 	{
-		T dbResult = tBase;
+		T tBase = tResult;
 		if (iExponent < 0)
 		{
 			throw "input error iExponent !";
@@ -68,29 +85,31 @@ T CStudyCPrimerPlus::Power(T tBase, int iExponent)
 
 		if (iExponent == 0)
 		{
-			dbResult = 1.0;
+			tResult = 1;
 		}
 		else
 		{
 			while (iExponent != 1)
 			{
-				dbResult = dbResult * tBase;
+				tResult = tResult * tBase;
 				--iExponent;
 			}
 		}
-		return tResult;
+		return STUDY_OK;
 
 	}
 	catch (const char* strError)
 	{
 		std::cout << strError << std::endl;
-		return 0.0;//当作错误码
+		return STUDY_FAIL;
 	}
 }
 
 template<typename T>
 void CStudyCPrimerPlus::ShowAndSize(const T& testVar)
 {
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::ShowAndSize");
+
 	std::cout << "The Test Value is: " << testVar << std::endl;
 	std::cout << "The Test Size is: " << sizeof(testVar) << std::endl;
 }
--- DataStructureAndAlgorithmDesignPatternsOfCpp/DataStructureAndAlgorithmDesignPatternsOfCpp/src/CPrimerPlus/CPrimerPlus.cpp
+++ DataStructureAndAlgorithmDesignPatternsOfCpp/DataStructureAndAlgorithmDesignPatternsOfCpp/src/CPrimerPlus/CPrimerPlus.cpp
@@ -7,45 +7,51 @@ const int CStudyCPrimerPlus::m_iConstStatic = 500;
 const std::string CStudyCPrimerPlus::m_strTestName = "Chongrui.Lu";
 
 
-//CProcessingData::CProcessingData(int& iSize) :m_iSize(iSize), m_iArray({}),
-//                                             m_iTestArray({}),m_iTestCall(0),m_iTestCallSecond(0),m_func(nullptr)
-//{
-//    //注意这种初始化与赋值是不一样的，
-//    //初始化列表不用再创建临时变量，从而更加高效
-//}
-
-CProcessingData::CProcessingData():m_iSize(10),m_pPublicClass(nullptr)
+CStudyCPrimerPlus::CStudyCPrimerPlus():m_iSize(0),m_iCout(0),m_listTest()
 {
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::CStudyCPrimerPlus");
 
 }
 
-CProcessingData::~CProcessingData()
+CStudyCPrimerPlus::CStudyCPrimerPlus(int iSize):m_iSize(iSize), m_iCout(0), m_listTest()
 {
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::CStudyCPrimerPlus(int iSize)");
 
 }
 
-
-void CProcessingData::ComputSizeOfVar()
+CStudyCPrimerPlus::CStudyCPrimerPlus(const CStudyCPrimerPlus& otherCPrimer):m_iSize(0)
 {
-	std::cout << "<--------------->" << std::endl;
-	std::cout << "ComputSizeOfVar" << std::endl;
-    int iTest = INT_MAX ;
-	m_pPublicClass->ShowAndSize<int>(iTest);
-    short sTest = SHRT_MAX;
-	m_pPublicClass->ShowAndSize<short>(sTest);
-    long  lTest = LONG_MAX;
-	m_pPublicClass->ShowAndSize<long>(lTest);
-    long long llTest = LLONG_MAX;
-	m_pPublicClass->ShowAndSize<long long>(llTest);
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::CStudyCPrimerPlus(const CStudyCPrimerPlus& otherCPrimer)");
+
+	m_iCout = otherCPrimer.m_iCout;
+}
+
+
+
+CStudyCPrimerPlus& CStudyCPrimerPlus::operator=(const CStudyCPrimerPlus& CStudyCPPObject)
+{
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::operator=");
+
+	//如果有动态内存，就要申请一个内存，赋值，消除内存，返回*this
+	if (this != &CStudyCPPObject)
+	{
+
+	}
+	return *this;
+}
+
+CStudyCPrimerPlus::~CStudyCPrimerPlus()
+{
+	PrintfTestFunctionFlag("CStudyCPrimerPlus::~CStudyCPrimerPlus()");
 
 }
+
 
 //can add const here,because it is does not change!
 //not ADT/UDT ,so not &
-void CProcessingData::SimpleFourArithmetic(double dbTestOne, double dbTestTwo, int iCalculationType)
+void CStudyCPrimerPlus::SimpleFourArithmetic(double dbTestOne, double dbTestTwo, int iCalculationType)
 {
-	std::cout << "<--------------->" << std::endl;
-	std::cout << "SimpleFourArithmetic" << std::endl;
+	PrintfTestFunctionFlag("SimpleFourArithmetic");
 	try
 	{
 		switch (iCalculationType)
@@ -101,8 +107,7 @@ void CProcessingData::SimpleFourArithmetic(double dbTestOne, double dbTestTwo, i
 //Implicit conversion and explicit conversion
 void CStudyCPrimerPlus::ClassToOtherClass()
 {
-	std::cout << "<--------------->" << std::endl;
-	std::cout << "ClassToOtherClass" << std::endl;
+	PrintfTestFunctionFlag("ClassToOtherClass");
 	//1. Implicit Conversion
 	//char is-a int ,int is-a long,long is-a float,float is-a double,Derived class is-a base class
 	//A is-a B
@@ -114,135 +119,88 @@ void CStudyCPrimerPlus::ClassToOtherClass()
 	//Promote the 100 of int to a temporary variable of double, 
 	//and then assign this temporary variable to dbB
 	char chA = 'C';
-	m_pPublicClass->ShowAndSize<char>(chA);
+	ShowAndSize<char>(chA);
 	int iB = chA;
-	m_pPublicClass->ShowAndSize<int>(iB);
+	ShowAndSize<int>(iB);
 	long lC = iB;
-	m_pPublicClass->ShowAndSize<long>(lC);
+	ShowAndSize<long>(lC);
 	float fA = 10.2;
-	m_pPublicClass->ShowAndSize<float>(fA);
+	ShowAndSize<float>(fA);
 	double dbC = fA;
-	m_pPublicClass->ShowAndSize<double>(dbC);
+	ShowAndSize<double>(dbC);
 
 
 	//2.Forced conversion
 	double dbG = 10.2;
-	m_pPublicClass->ShowAndSize<double>(dbG);
+	ShowAndSize<double>(dbG);
 	int iH = (int)dbG;
-	m_pPublicClass->ShowAndSize<int>(iH);
+	ShowAndSize<int>(iH);
 
 }
 
-void CProcessingData:: StudyConstChar(const char* pConstCharConst,std::string strName)
+void CStudyCPrimerPlus:: StudyConstChar(const char* pConstCharConst,std::string strName)
 {
-	std::cout << "<--------------->" << std::endl;
-	std::cout << "StudyConstChar" << std::endl;
+	PrintfTestFunctionFlag("StudyConstChar");
 
 	//const char* can not inital char*
 	//char* pCh = "I am SB!";
 	strName = pConstCharConst;
 	std::string strToConstChar = "strToConstChar";
-	m_pPublicClass->ShowAndSize<std::string>(strToConstChar);
+	ShowAndSize<std::string>(strToConstChar);
 
 	const char* pFromString = strToConstChar.c_str();
-	m_pPublicClass->ShowAndSize<const char*>(pFromString);
+	ShowAndSize<const char*>(pFromString);
 
 	pFromString = "I am very important!!";
-	m_pPublicClass->ShowAndSize<const char*>(pFromString);
+	ShowAndSize<const char*>(pFromString);
 
 	//string cann't  direct conversion to char 
 	//string to char*
 	std::string strToConstPtrChar = "stringToConstPtrChar";
 	char* pCharFromSting = const_cast<char*>(strToConstPtrChar.c_str());
-	m_pPublicClass->ShowAndSize<char*>(pCharFromSting);
+	ShowAndSize<char*>(pCharFromSting);
 
 }
 
-
-//All commented out are wrong
-void CFourthComplexDataType::TestFourthComplexDataType()
-{
-	std::cout << "test this void CFourthComplexDataType::GetMemory(char* pChar, int iNum)" << std::endl;
-	size_t iSize = 0;
-	char* pChar = nullptr;
-	GetMemory(pChar, 100);//pChar ��Ȼ����nullptr
-	iSize = sizeof(pChar);
-	std::cout << "pChar size is :" << iSize << std::endl;
-	//std::strcpy(pChar,"hello");//����ʱ�����why?
-
-	//std::cout << "test this void CFourthComplexDataType::GetMemory(char** pChar, int iNum)" << std::endl;
-	//char* pStr = nullptr;
-	//GetMemory(&pStr, 100);
-	//iSize = sizeof(pStr);
-	//std::cout << "pStr size is :" << iSize << std::endl;
-	//strcpy(pStr, "hello");//this is ok
-	//iSize = sizeof(pStr);
-	//std::cout << "after strcpy pStr size is :" << iSize << std::endl;
-	//std::cout << "after strcpy Value of pStr is :" << pStr << std::endl;
-	//free(pStr);//�ɶԳ���malloc/free
-
-	//std::cout << "test this char* CFourthComplexDataType::GetMemory(int inum)" << std::endl;
-	//char* pChar3 = nullptr;
-	//pChar3 = GetMemory(100);
-	//iSize = sizeof(pChar3);
-	//std::cout << "before strcpy pChar3 size is :" << iSize << std::endl;
-	//strcpy(pChar3,"hellolcr");
-	//iSize = sizeof(pChar3);
-	//std::cout << "after strcpy pChar3 size is :" << iSize << std::endl;
-	//std::cout << "after strcpy Value of pChar3  is :" << pChar3 << std::endl;
-	//free(pChar3);
-
-	//std::cout << "test this char* CFourthComplexDataType::GetString(void)" << std::endl;
-	//char* pCharGetString = nullptr;
-	//pCharGetString = GetString();
-	//iSize = sizeof(pCharGetString);
-	//std::cout << "PCharGetString size is :" << iSize << std::endl;
-	//std::cout << "Value of PCharGetString  is :" << pCharGetString << std::endl;
-
-	//std::cout << "test this char* CFourthComplexDataType::GetStringSecond(void)" << std::endl;
-	//char* pCharGetStringSecond = nullptr;
-	//pCharGetStringSecond = GetStringSecond();
-	//iSize = sizeof(pCharGetStringSecond);
-	//std::cout << "pCharGetStringSecond size is :" << iSize << std::endl;
-	//std::cout << "Value of pCharGetStringSecond  is :" << pCharGetStringSecond << std::endl;
-
-}
 
 //this is wrong ,pChar will create a copier ,so get a wrong ptr!
-void CFourthComplexDataType::GetMemory(char* pChar, int iNum)
+//类似一个传值参数，不会变化的
+void CStudyCPrimerPlus::GetMemory(char* pChar, int iNum)
 {
-	pChar = (char*)std::malloc(sizeof(char) * iNum);
-}
-//this is right
-void CFourthComplexDataType::GetMemory(char** pChar, int iNum)
-{
-	*pChar = (char*)std::malloc(sizeof(char) * iNum);
+	PrintfTestFunctionFlag(" CStudyCPrimerPlus::GetMemory");
+	pChar = new char[iNum];
 }
 
-//this is wrong!! get a wild ptr
-//delete a temporary variables，ptr donot delete
-char* CFourthComplexDataType::GetMemory(int inum)
+//this is right
+//指针传递
+//使用的时候是
+//char* pChar ; 
+//(&pChar,10)
+void CStudyCPrimerPlus::GetMemory(char** pChar, int iNum)
 {
-	char* pChar = (char*)std::malloc(sizeof(char) * inum);
+	PrintfTestFunctionFlag(" CStudyCPrimerPlus::GetMemory");
+	*pChar = new char[iNum];
+}
+
+//this is right
+char* CStudyCPrimerPlus::GetMemory(int inum)
+{
+	PrintfTestFunctionFlag(" CStudyCPrimerPlus::GetMemory");
+	char* pChar = new char[inum];
+	memset(pChar, 0, sizeof(char) * inum);
 	return pChar;
 }
 
-char* CFourthComplexDataType::GetString(void)
+//this is wrong!! get a wild ptr
+char* CStudyCPrimerPlus::GetString(void)
 {
+	PrintfTestFunctionFlag(" CStudyCPrimerPlus::GetString");
 	char pCharArray[] = "hello world";
 	//返回的是临时变量的地址是有问题的！
 	return pCharArray;
 }
 
-//char* CFourthComplexDataType::GetStringSecond(void)
-//{
-//	//char* pChar = "hello world";//�����д���
-//	//return pChar;
-//}
-
-#include "CPrimerPlus/CPublicClass.h"
-
-void CPublicClass::PrintfTestFunctionFlag(std::string strFunctionNmae)
+void CStudyCPrimerPlus::PrintfTestFunctionFlag(std::string strFunctionNmae)
 {
 	std::cout << "<------------------------------------>" << std::endl;
 	std::cout << strFunctionNmae << std::endl;
