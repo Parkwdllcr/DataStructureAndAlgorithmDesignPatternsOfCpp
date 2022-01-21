dbe583491025a297d37bd11513ec042fee9276f0
Parkwdllcr <1441641216@qq.com>
2022-01-21 11:33:20 +0800

    add Cthread

---
+++ DataStructureAndAlgorithmDesignPatternsOfCpp/ThreadActor/ListAndMap/CThread.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+class CThread
+{
+public:
+	CThread();
+	virtual ~CThread();
+
+	enum State
+	{
+		Stoped,     ///<停止状态，包括从未启动过和启动后被停止
+		Running,    ///<运行状态
+		Paused      ///<暂停状态
+	};
+	State state() const;
+
+	void start();
+	void stop();
+	void pause();
+	void resume();
+
+protected:
+	virtual void process() = 0;
+
+private:
+	void run();
+
+private:
+	std::thread* _thread;
+	std::mutex _mutex;
+	std::condition_variable _condition;
+	std::atomic_bool _pauseFlag;   ///<暂停标识
+	std::atomic_bool _stopFlag;   ///<停止标识
+	State _state;
+};
+
+
+
